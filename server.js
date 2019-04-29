17dac9daab536668324be809dbed171385b80ed3
Sara Mayberry <saraamayberry@yahoo.com>
2019-04-29 12:22:27 -0600

    deploy-fixes

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const MOVIES = require('./moviedex-data.json')
 const cors = require('cors')
 const helmet = require('helmet')
 const app = express()
-const PORT = 8000
-
-app.use(morgan('dev'))
+const PORT = process.env.PORT || 8000
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
 
@@ -16,12 +16,20 @@ app.use(function validateBearerToken(req,res,next) {
     const authToken = req.get('Authorization')
     const apiToken = process.env.API_TOKEN
 
-    console.log('validate bearer token')
-
     if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'})
     }
     next()
+})
+
+app.use((error, req, res, next) => {
+    let response 
+    if (process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error'}}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
 })
 
 app.get('/movie', function handleGetMovie(req, res) {
