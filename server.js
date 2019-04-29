6dcc45e75916934831ee793c6f41923630977459
Sara Mayberry <saraamayberry@yahoo.com>
2019-04-29 08:22:06 -0600

    auth-header

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const PORT = 8000
+
+
+const validTypes = ['filmtv_ID', 'film_title', 'year', 'genre', 'duration', 'country', 'director', 'actors', 'avg_vote', 'votes']
+
+app.use(morgan('dev'))
+
+app.use(function validateBearerToken(req,res,next) {
+    const bearerToken = req.get('Authorization').split(' ')[1]
+    const apiToken = process.env.API_TOKEN
+
+    console.log('validate bearer token')
+
+    if (bearerToken !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'})
+    }
+    next()
+})
+
+app.get('/movie', function handleGetMovie(req, res) {
+    res.json(validTypes)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`)
+})
